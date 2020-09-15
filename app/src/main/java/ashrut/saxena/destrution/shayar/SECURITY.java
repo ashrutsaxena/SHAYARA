14e1001c34ffb0113704afec85032b0e9205e236
ashrut saxena <ashrut.saxena_cs18@gla.ac.in>
2020-09-15 12:36:27 +0700

    SHAYRANA PROJECT

--- app/src/main/java/ashrut/saxena/destrution/shayar/SECURITY.java
+++ app/src/main/java/ashrut/saxena/destrution/shayar/SECURITY.java
@@ -1,5 +1,6 @@
 package ashrut.saxena.destrution.shayar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -13,12 +14,16 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseException;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.PhoneAuthCredential;
 import com.google.firebase.auth.PhoneAuthProvider;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.rilixtech.widget.countrycodepicker.CountryCodePicker;
 
 import java.util.concurrent.TimeUnit;
@@ -33,6 +38,7 @@ public class SECURITY extends AppCompatActivity {
     String verificationid;
     PhoneAuthProvider.ForceResendingToken token;
     Boolean verificationinprogress = false;
+    FirebaseFirestore fstore;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,6 +51,7 @@ public class SECURITY extends AppCompatActivity {
         nextbtn = findViewById(R.id.nextBtn);
         state = findViewById(R.id.state);
         codePicker = findViewById(R.id.ccp);
+        fstore = FirebaseFirestore.getInstance();
 
         nextbtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -79,16 +86,48 @@ public class SECURITY extends AppCompatActivity {
         });
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (fAuth.getCurrentUser() != null) {
+            progressBar.setVisibility(View.VISIBLE);
+            state.setText("Checking..");
+            state.setVisibility(View.VISIBLE);
+            checkuserprofile();
+        }
+    }
+
+
+
     private void verifyAuth(PhoneAuthCredential credential) {
         fAuth.signInWithCredential(credential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if (task.isSuccessful()) {
-                    Toast.makeText(SECURITY.this, "Authentication is Successful", Toast.LENGTH_SHORT).show();
+                    checkuserprofile();
+
                 } else {
                     Toast.makeText(SECURITY.this, "Authentication is Failed", Toast.LENGTH_SHORT).show();
 
                 }
+            }
+        });
+    }
+
+    private void checkuserprofile() {
+        DocumentReference docref = fstore.collection("users").document(fAuth.getCurrentUser().getUid());
+        docref.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                if (documentSnapshot.exists()) {
+                    startActivity(new Intent(getApplicationContext(), Main2Activity.class));
+                    finish();
+                } else {
+                    startActivity(new Intent(getApplicationContext(), AUTH.class));
+                    finish();
+                }
+
             }
         });
     }
